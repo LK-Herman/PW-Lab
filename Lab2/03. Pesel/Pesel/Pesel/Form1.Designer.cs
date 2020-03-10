f39eec1a7f38eec1960f37b08f3b99ac2a18437d
Łukasz <61921500+LK-Herman@users.noreply.github.com>
2020-03-10 19:53:59 +0100

    Add files via upload
    Weryfikator nr PESEL

---
+++ Lab2/03. Pesel/Pesel/Pesel/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Pesel
+{
+    partial class FormPesel
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxPesel = new System.Windows.Forms.TextBox();
+            this.buttonSprawdz = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.labelWynik = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(100, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "WERYFIKACJA NR PESEL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nr PESEL:";
+            // 
+            // textBoxPesel
+            // 
+            this.textBoxPesel.Location = new System.Drawing.Point(55, 102);
+            this.textBoxPesel.Name = "textBoxPesel";
+            this.textBoxPesel.Size = new System.Drawing.Size(304, 20);
+            this.textBoxPesel.TabIndex = 2;
+            // 
+            // buttonSprawdz
+            // 
+            this.buttonSprawdz.Location = new System.Drawing.Point(55, 177);
+            this.buttonSprawdz.Name = "buttonSprawdz";
+            this.buttonSprawdz.Size = new System.Drawing.Size(122, 32);
+            this.buttonSprawdz.TabIndex = 3;
+            this.buttonSprawdz.Text = "Sprawdź";
+            this.buttonSprawdz.UseVisualStyleBackColor = true;
+            this.buttonSprawdz.Click += new System.EventHandler(this.buttonSprawdz_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(237, 177);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(122, 32);
+            this.buttonClose.TabIndex = 4;
+            this.buttonClose.Text = "Wyjdź";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // labelWynik
+            // 
+            this.labelWynik.BackColor = System.Drawing.Color.White;
+            this.labelWynik.Location = new System.Drawing.Point(55, 137);
+            this.labelWynik.Name = "labelWynik";
+            this.labelWynik.Size = new System.Drawing.Size(304, 25);
+            this.labelWynik.TabIndex = 5;
+            this.labelWynik.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // FormPesel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 248);
+            this.Controls.Add(this.labelWynik);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonSprawdz);
+            this.Controls.Add(this.textBoxPesel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormPesel";
+            this.Text = "PESEL weryfikacja";
+           
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxPesel;
+        private System.Windows.Forms.Button buttonSprawdz;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Label labelWynik;
+    }
+}
+
---
+++ Lab2/03. Pesel/Pesel/Pesel/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pesel
+{
+    public partial class FormPesel : Form
+    {
+        public FormPesel()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonSprawdz_Click(object sender, EventArgs e)
+        {
+            // SPRAWDZENIE: <11; >11; czy jest liczbą
+            if (textBoxPesel.Text.Length < 11)
+            {
+                labelWynik.BackColor = Color.Crimson;
+                labelWynik.ForeColor = Color.White;
+                labelWynik.Text = "Za krótki nr PESEL";
+
+            }
+            else
+            {
+                if (textBoxPesel.Text.Length > 11)
+                {
+                    labelWynik.BackColor = Color.Crimson;
+                    labelWynik.ForeColor = Color.White;
+                    labelWynik.Text = "Za długi nr PESEL";
+                }
+                else
+                {
+                    int checkDigit = 0;         //licznik blednych znaków 
+                    string bledneZnaki = "";    //zbiera błędne znaki
+
+                    foreach (var ch in textBoxPesel.Text)  //sprawdza każdy znak, czy jest liczbą
+                    {
+                        if (Char.IsDigit(ch) == false)
+                        {
+                            checkDigit++;
+                            if (ch == ' ') bledneZnaki = bledneZnaki + " spacja";   //wyjątek dla "niewidocznej" spacji
+                            else bledneZnaki = bledneZnaki + " " + ch.ToString();
+                        }
+                    }
+                    if (checkDigit != 0)  //jesli istnieją błędne znaki
+                    {
+                        labelWynik.BackColor = Color.Crimson;
+                        labelWynik.ForeColor = Color.White;
+                        labelWynik.Text = "Wprowadzono błędnych znaków: " + checkDigit + "  (" + bledneZnaki + ")";
+                    }
+                    //SPRAWDZENIE: Koniec
+                    else  //Jesli pesel jest 11cyfrową liczbą to:
+                    {
+                        labelWynik.Text = "";
+                        char[] peselCH = textBoxPesel.Text.ToCharArray();
+                        int[] pes = new int[peselCH.Length];
+
+                        for (int x = 0; x < 11; x++) pes[x] = Convert.ToInt32(Convert.ToString(peselCH[x]));
+
+                        int wzor1 = pes[0] + 3 * pes[1] + 7 * pes[2] + 9 * pes[3] + pes[4] + 3 * pes[5] + 7 * pes[6] + 9 * pes[7] + pes[8] + 3 * pes[9];
+
+                        wzor1 = wzor1 - ((wzor1 / 100) * 100);
+                        wzor1 = wzor1 - ((wzor1 / 10) * 10);
+                        wzor1 = 10 - wzor1;
+
+                        if (wzor1 == pes[10])
+                        {
+                            labelWynik.BackColor = Color.DarkGreen;
+                            labelWynik.ForeColor = Color.White;
+                            labelWynik.Text = "Nr PESEL jest prawidłowy.";
+                        }
+                        else
+                        {
+                            labelWynik.BackColor = Color.Crimson;
+                            labelWynik.ForeColor = Color.White;
+                            labelWynik.Text = "Niestety podany nr PESEL jest nieprawidłowy.";
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
