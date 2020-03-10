47d6eef5584387821684a51581f31e6b2726c565
Łukasz <61921500+LK-Herman@users.noreply.github.com>
2020-03-10 19:49:42 +0100

    Add files via upload
    Kalkulator BMI

---
+++ Lab2/01. BMI/01. BMI/BMIwyklad/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMIwyklad
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((textBox1.Text != "") && (textBox2.Text != ""))
+            {
+                label3.ForeColor = Color.White;
+                double waga = Convert.ToDouble(textBox1.Text);
+                double wzrost = Convert.ToDouble(textBox2.Text);
+                //Debug.WriteLine(waga);
+                //Debug.WriteLine(wzrost);
+                double bmi = (waga / (wzrost * wzrost));
+                label3.Text = bmi.ToString();
+            }
+            else
+            {
+                label3.Text = "Brak danych";
+                label3.ForeColor = Color.Red;            
+            }
+        }
+
+       
+    }
+}
